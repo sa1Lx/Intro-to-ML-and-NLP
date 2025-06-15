20d3ba219fafbde50d65e93e27c6e47e9ac3a9cd
Sailesh Kumar Sahoo <contactsaileshsahoo@gmail.com>
2025-06-15 17:29:29 +0530

    changes

--- Week_1/Week_1.py
+++ Week_1/Week_1.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-arr = np.random.randint(-20, 20, (5,4))
+arr = np.random.randint(1, 50, (5,4))
 print(arr)
+
+j = 3
+
+for row in arr:
+    print(row[j])
+    j-= 1
+    if j < 0:
+        break
